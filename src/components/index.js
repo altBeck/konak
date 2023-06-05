a80df4319e94d1337eeddabcd056f90bada13998
Kanno Beck <drkannobeck@gmail.com>
2023-06-05 18:24:49 +0100

    update-setup-files

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as Nav } from './Nav/Nav';
-export { default as Header } from './header/Header';
-export { default as About } from './about/About';
-export { default as Services } from './services/Services';
-export { default as Projects } from './projects/Projects';
-export { default as Footer } from './footer/Footer';
+export { default as Header } from './Header/Header';
+export { default as About } from './About/About';
+export { default as Services } from './Services/Services';
+export { default as Projects } from './Projects/Projects';
+export { default as Footer } from './Footer/Footer';
